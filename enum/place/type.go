50e3e3a9b37b52e98e02406baa32bf25b89dbe51
Yosef da Iry <yosefda@gmail.com>
2020-05-09 13:07:42 +1000

    Create place enum type Implement place description model

---
+++ enum/place/type.go
@@ -0,0 +1,8 @@
+// Package place provides enumerate types related to places.
+// There is no current definition of a set of known place types.
+package place
+
+import "github.com/yosefda/gedcomx/typedef"
+
+// Type enum type.
+type Type typedef.URI
