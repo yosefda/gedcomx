22dc567050dff3d9eaf3f4093d3ced20d81f5389
Yosef da Iry <yosefda@gmail.com>
2020-05-09 13:46:17 +1000

    Implement fact model

--- conceptual/component/fact/fact.go
+++ conceptual/component/fact/fact.go
@@ -1,1 +1,22 @@
+// Package fact defines a data item that is presumed to be true about a specific subject, such as a person or relationship
+// https://github.com/FamilySearch/gedcomx/blob/master/specifications/conceptual-model-specification.md#314-the-fact-data-type
 package fact
+
+import (
+	"github.com/yosefda/gedcomx/conceptual/component/placereference"
+	"github.com/yosefda/gedcomx/conceptual/component/qualifier"
+	"github.com/yosefda/gedcomx/enum/fact"
+	"github.com/yosefda/gedcomx/typedef"
+)
+
+// URI is the identifier for the Fact data type.
+const URI = "http://gedcomx.org/v1/Fact"
+
+// Fact data type.
+type Fact struct {
+	Type       *fact.Fact
+	Date       typedef.Date
+	Place      *placereference.PlaceReference
+	Value      string
+	Qualifiers *[]qualifier.Qualifier
+}
